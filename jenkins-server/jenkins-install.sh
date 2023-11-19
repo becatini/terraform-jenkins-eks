b26f27f7dc0a44eaae2558ba0244e0466f240ce5
becatini <fbecatini@gmail.com>
2023-11-19 06:46:19 -0700

    v1

--- jenkins-server/jenkins-install.sh
+++ jenkins-server/jenkins-install.sh
@@ -7,7 +7,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 #sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install java-17-amazon-corretto-headless
+sudo yum install java-17-amazon-corretto-headless -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
